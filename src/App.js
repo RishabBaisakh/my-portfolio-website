001e9390276e1b160b130336770de883af4bc523
RishabBaisakh <rishab.baisakh@gmail.com>
2020-12-04 16:37:04 -0500

    added react router dom, and added paths

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import About from "./About";
+import Skills from "./Skills";
+import Education from "./Education";
+import Work from "./Work";
+import Experience from "./Education";
+import Contact from "./Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/skills/">
+            <Skills />
+          </Route>
+          <Route path="/education/">
+            <Education />
+          </Route>
+          <Route path="/experience/">
+            <Experience />
+          </Route>
+          <Route path="/work/">
+            <Work />
+          </Route>
+          <Route>
+            <Contact />
+          </Route>
+          <Route path="/">
+            <About />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
